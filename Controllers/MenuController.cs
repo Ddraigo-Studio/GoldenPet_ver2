f8d0d8efe21466d87ad2ed575d7dcdd4b5ce01b3
Ddraigo <laithanh1000@gmai.com>
2024-09-21 01:23:36 +0700

    add dynamic menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using GoldenPet.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GoldenPet.Controllers
+{
+    public class MenuController : Controller
+    {
+        GoldenPetEntities _db = new GoldenPetEntities();
+
+        // GET: Memu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetMenu()
+        {
+            var v = from t in _db.tb_Menu
+                    where t.hide == true
+                    orderby t.order ascending
+                    select t;
+
+            return PartialView(v.ToList());
+        }
+    }
+}
