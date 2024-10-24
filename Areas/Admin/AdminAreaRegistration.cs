c8e14a4725a3a6e56b725488617c8973a1388b70
Huy Duong <huyduong11082004@gmail.com>
2024-10-24 19:40:48 +0700

    Admin

---
+++ Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace GoldenPet.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { Controller = "Default", action ="Index", id = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
+                "Menu",
+                "Menu/{controller}/{action}/{id}",
+                new { Controller = "Menu", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GoldenPet.Models;
+
+namespace GoldenPet.Areas.Admin.Controllers
+{
+    public class MenuController : Controller
+    {
+        private GoldenPetEntities db = new GoldenPetEntities();
+
+        // GET: Admin/Menu
+        public ActionResult Index()
+        {
+            return View(db.tb_Menu.ToList());
+        }
+
+        // GET: Admin/Menu/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_Menu tb_Menu = db.tb_Menu.Find(id);
+            if (tb_Menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_Menu);
+        }
+
+        // GET: Admin/Menu/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Menu/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,link,meta,hide,order,datebegin")] tb_Menu tb_Menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_Menu.Add(tb_Menu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tb_Menu);
+        }
+
+        // GET: Admin/Menu/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_Menu tb_Menu = db.tb_Menu.Find(id);
+            if (tb_Menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_Menu);
+        }
+
+        // POST: Admin/Menu/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,link,meta,hide,order,datebegin")] tb_Menu tb_Menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tb_Menu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tb_Menu);
+        }
+
+        // GET: Admin/Menu/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_Menu tb_Menu = db.tb_Menu.Find(id);
+            if (tb_Menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_Menu);
+        }
+
+        // POST: Admin/Menu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tb_Menu tb_Menu = db.tb_Menu.Find(id);
+            db.tb_Menu.Remove(tb_Menu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
